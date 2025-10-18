03b9954604a0a7b6715a4073a941399e5b0fa34e
bendaikh <bendaikh2000@gmail.com>
2025-10-18 18:52:58 +0100

    commited

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,9 @@ Route::middleware('domain-check')->group(function () {
     Route::get('pricing', [HomeController::class, 'Pricing'])->name('apps.pricing');
     Route::get('pricing/plans', [HomeController::class, 'PricingPlans'])->name('apps.pricing.plan');
     Route::get('pages', [HomeController::class, 'CustomPage'])->name('custompage');
-    Route::get('/', [HomeController::class, 'index'])->name('start');
+    Route::get('/', function () {
+        return redirect()->route('login');
+    })->name('start');
 });
 Route::middleware(['auth', 'verified'])->group(function () {
 
